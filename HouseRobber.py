d7a4b61da779c34a534332d193efa52765cc33e7
bryanmax9 <brillant16.gcv@gmail.com>
2024-08-20 23:57:21 -0700

    💀😮🫠done some leetcode

--- HouseRobber.py
+++ HouseRobber.py
@@ -34,16 +34,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
+        rob1, rob2 = 0, 0
 
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
-
-        return dp[-1]
+        for n in nums:
+            temp = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
